83e34a1ebc2c8978ca9c3f2d4264528593489a48
Artem Eroshenko <eroshenkoam@qameta.io>
2016-08-25 12:52:42 +0300

    Add hover method

--- src/main/java/io/qameta/htmlelements/element/HtmlElement.java
+++ src/main/java/io/qameta/htmlelements/element/HtmlElement.java
@@ -1,10 +1,17 @@
 package io.qameta.htmlelements.element;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.internal.Locatable;
 
 public interface HtmlElement extends WebBlock, WebElement, Locatable,
         ShouldMatched<HtmlElement>,
         WaitUntilMatched<HtmlElement> {
+
+    default HtmlElement hover() {
+        Actions actions = new Actions(getContext().getDriver());
+        actions.moveToElement(this);
+        return this;
+    }
 
 }
