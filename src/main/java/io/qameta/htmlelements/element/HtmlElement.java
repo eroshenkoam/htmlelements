283f948ea83f3623fb27e4a566ee343d268178de
Artem Eroshenko <eroshenkoam@qameta.io>
2016-09-04 21:01:37 +0300

    Some refactoring

--- src/main/java/io/qameta/htmlelements/element/HtmlElement.java
+++ src/main/java/io/qameta/htmlelements/element/HtmlElement.java
@@ -1,14 +1,6 @@
 package io.qameta.htmlelements.element;
 
-import io.qameta.htmlelements.extension.DescriptionProvider;
-import io.qameta.htmlelements.extension.SelectorProvider;
-
 public interface HtmlElement extends ExtendedWebElement<HtmlElement> {
 
-    @SelectorProvider
-    String getSelector();
-
-    @DescriptionProvider
-    String getDescription();
 
 }
--- src/main/java/io/qameta/htmlelements/extension/ExtensionRegistry.java
+++ src/main/java/io/qameta/htmlelements/extension/ExtensionRegistry.java
@@ -4,7 +4,6 @@ import io.qameta.htmlelements.util.ReflectionUtils;
 
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -15,13 +14,13 @@ import java.util.stream.Collectors;
 public class ExtensionRegistry {
 
     public static ExtensionRegistry create(Class<?> extensionClass) {
-        List<Extension> extensions = new ArrayList<>();
+        ExtensionRegistry registry = new ExtensionRegistry();
+
         ReflectionUtils.getMethods(extensionClass)
                 .forEach(method -> Arrays.stream(method.getAnnotations())
                         .filter(annotation -> annotation.annotationType().isAnnotationPresent(ExtendWith.class))
                         .map(annotation -> annotation.annotationType().getAnnotation(ExtendWith.class).value())
-                        .map(ReflectionUtils::newInstance)
-                        .forEach(extensions::add));
+                        .forEach(registry::registerExtension));
 
         Map<Method, MethodHandler> handlers = new HashMap<>();
         ReflectionUtils.getMethods(extensionClass).forEach(method -> {
@@ -32,34 +31,36 @@ public class ExtensionRegistry {
             });
         });
 
-        ExtensionRegistry registry = new ExtensionRegistry();
-        registry.getExtensions().addAll(extensions);
         registry.getHandlers().putAll(handlers);
         return registry;
     }
 
-    private final List<Extension> extensions;
+    private final Map<Class<? extends Extension>, Extension> extensions;
 
     private final Map<Method, MethodHandler> handlers;
 
     private ExtensionRegistry() {
-        this.extensions = new ArrayList<>();
+        this.extensions = new HashMap<>();
         this.handlers = new HashMap<>();
     }
 
-    private List<Extension> getExtensions() {
+    public void registerExtension(Class<? extends Extension> extensionType) {
+        getExtensions().putIfAbsent(extensionType, ReflectionUtils.newInstance(extensionType));
+    }
+
+    private Map<Class<? extends Extension>, Extension> getExtensions() {
         return extensions;
+    }
+
+    public <T extends Extension> List<T> getExtensions(Class<T> extensionType) {
+        return getExtensions().values().stream()
+                .filter(extension -> extensionType.isAssignableFrom(extension.getClass()))
+                .map(extensionType::cast)
+                .collect(Collectors.toList());
     }
 
     private Map<Method, MethodHandler> getHandlers() {
         return handlers;
-    }
-
-    public <T extends Extension> List<T> getExtensions(Class<T> extensionType) {
-        return getExtensions().stream()
-                .filter(extension -> extensionType.isAssignableFrom(extension.getClass()))
-                .map(extensionType::cast)
-                .collect(Collectors.toList());
     }
 
     public Optional<? extends MethodHandler> getHandler(Method method) {
