6c82389493b79df31fac25d304ca591d80815772
Алексей Заровный <Nogert@yandex.ru>
2017-09-16 19:34:46 +0300

    fix global parameters for custom blocks (via #21)

--- src/test/java/io/qameta/htmlelements/GlobalParametersTest.java
+++ src/test/java/io/qameta/htmlelements/GlobalParametersTest.java
@@ -24,13 +24,18 @@ public class GlobalParametersTest {
         WebDriver driver = TestData.mockDriver();
         WebElement element = TestData.WebElementBuilder.mockWebElement().withDisplayed(true).build();
         when(driver.findElement(By.xpath("//div[@class='value']"))).thenReturn(element);
+        when(driver.findElement(By.xpath("//div[@class='simpleElement']"))).thenReturn(element);
+        when(element.findElement(By.xpath(".//div[@class='childElement']"))).thenReturn(element);
 
         WebPageFactory factory = new WebPageFactory()
-                .parameter("key", "value");
+                .parameter("key", "value")
+                .parameter("simple", "simpleElement")
+                .parameter("child", "childElement");
 
         SimplePage page = factory.get(driver, SimplePage.class);
         page.input().should(DisplayedMatcher.displayed());
-
+        page.simpleElement().should(DisplayedMatcher.displayed());
+        page.simpleElement().childElement().should(DisplayedMatcher.displayed());
     }
 
     public interface SimplePage extends WebPage {
@@ -38,8 +43,14 @@ public class GlobalParametersTest {
         @FindBy("//div[@class='{{ key }}']")
         HtmlElement input();
 
+        @FindBy("//div[@class='{{ simple }}']")
+        SimpleElement simpleElement();
     }
 
+    public interface SimpleElement extends HtmlElement {
 
+        @FindBy(".//div[@class='{{ child }}']")
+        HtmlElement childElement();
+    }
 
 }
