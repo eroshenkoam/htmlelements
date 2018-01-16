178967a94041644904c8780e10ccd98503e95608
igor.martynov <igor.martynov@team.wrike.com>
2018-01-16 15:23:21 +0300

    Up selenium version and switch test to jupiter (Junit 5)

--- src/test/java/io/qameta/htmlelements/GlobalParametersTest.java
+++ src/test/java/io/qameta/htmlelements/GlobalParametersTest.java
@@ -4,13 +4,11 @@ import io.qameta.htmlelements.annotation.FindBy;
 import io.qameta.htmlelements.element.HtmlElement;
 import io.qameta.htmlelements.example.TestData;
 import io.qameta.htmlelements.matcher.DisplayedMatcher;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.when;
 
 /**
